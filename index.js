6d8c8a6e5ab83e4382f134243c1b42941ca12f7c
Elijah Elliott <elliott.t.elijah@gmail.com>
2024-02-01 14:37:43 -0700

    added "@react-native-firebase/app": "^18.8.0",     "@react-native-firebase/auth": "^18.8.0",     "@react-native-firebase/firestore": "^18.8.0",     "firebase": "^10.7.2",

--- index.js
+++ index.js
@@ -5,5 +5,9 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { initializeApp } from '@react-native-firebase/app';
+
+// Initialize Firebase
+initializeApp();
 
 AppRegistry.registerComponent(appName, () => App);
