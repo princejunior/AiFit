65a102b8397d1845248b02b693d155de6aa540eb
Elijah Elliott <elliott.t.elijah@gmail.com>
2024-02-05 09:48:51 -0700

    i 've added a .gitignore file and a firebaseConfig.js file to the project. For others wishing to replicate the setup, it's necessary to create a .env file and incorporate the Firebase configurations

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import { initializeApp } from '@react-native-firebase/app';
+import firebaseConfig from './Firebase/firebaseConfig';
 
 // Initialize Firebase
 initializeApp();
